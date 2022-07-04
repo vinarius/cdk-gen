4c94f737bf526c10809d6940e5e494aae960224e
Mark Kraus <vindevccm@gmail.com>
2022-07-04 17:05:24 -0400

    debugging

--- main.ts
+++ main.ts
@@ -1,12 +1,9 @@
-import { cpSync, readdirSync } from 'fs';
+import { cpSync } from 'fs';
+import { resolve } from 'path';
 
 export function main(): void {
-  console.log('__dirname:', __dirname);
-  console.log('process.cwd():', process.cwd());
-  console.log('readdirSync(__dirname):', readdirSync(__dirname));
-
   cpSync(
-    './template',
+    resolve(__dirname, 'template'),
     process.argv[2] ?? process.cwd(),
     { recursive: true }
   );
